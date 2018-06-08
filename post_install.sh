f7b9a120367019097abac2888dd4ffc23cd04e27
Martin Wilke <miwi@FreeBSD.org>
2018-06-08 23:53:56 +0800

    - Added php-fpm - Added mysql - Added nginx

--- post_install.sh
+++ post_install.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
 # Enable the service
-sysrc -f /etc/rc.conf ttrss_enable="YES"
+sysrc -f /etc/rc.conf nginx_enable="YES"
+sysrc -f /etc/rc.conf php-fpm_enable="YES"
+sysrc -f /etc/rc.conf mysql_enable="YES"
+
+
+
 
 # Start the service
-service ttrss start 2>/dev/null
+service mysql start 2>/dev/null
+service php-fpm start 2>/dev/null
+service nginx start 2>/dev/null
+
